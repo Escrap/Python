611e140d211497b4bdafd19181624d157fe8c6f4
Escrap <ericmcclary82@gmail.com>
2022-11-16 08:00:08 -0500

    add main

--- Value_Error.py
+++ Value_Error.py
@@ -26,29 +26,31 @@
 # Reprompt Break with loop
 # Add in a loop to prompt user for x as an int
 
-while True:
-    try:
-        x = int(input("What's X? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
-
-print(f"x is {x}")
+#while True:
+#    try:
+#        x = int(input("What's X? "))
+#    except ValueError:
+#        print("x is not an integer")
+#    else:
+#        break
+#
+#print(f"x is {x}")
 
 # What if you want more numbers later
 # Create your own function get_int
 
-#def main():
-#    x = get_int()
-#    print(f"x is {x}")
-#
-#def get_int():
-#    while True:
-#        try:
-#            x = int(input("What's X? "))
-#        except ValueError:
-#            print("x is not an integer")
-#        else:
-#            break
-#    return x
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's X? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            
+            return x
+main()
+# above is not running and I don't know why remember to call main
